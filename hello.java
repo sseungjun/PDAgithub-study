02ededc1a2a38b11312404cf097e1ec854241a97
GamGomYang <ay6656@naver.com>
2025-05-25 15:21:08 +0900

    feat/hello.java 기능 추가

--- hello.java
+++ hello.java
@@ -2,29 +2,13 @@ import java.util.Scanner;
 
 public class hello{
 
-    public void sqrt(int input){
-
-        int total = 1;
-
-        for(int i=0 ; i<input ; i++ ){
-
-            total *= input;
-
-            
-        }
-    }
-
-
-
     public static void main(String[] args) {
         
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("숫자를 입력하세요 : ");
-
-        int input = scanner.nextInt();
+        String name = scanner.nextLine();
 
 
+        System.out.println(name + " hello git ");
     }
 }
