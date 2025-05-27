d2a909650f0e154a61bdb782359b4de0fb682344
GamGomYang <ay6656@naver.com>
2025-05-27 14:07:14 +0900

    github action test

--- gamgomyang/hello.py
+++ gamgomyang/hello.py
@@ -1,3 +1,11 @@
-# hello.py
+"""main.py - 간단한 파일 입출력을 수행하는 모듈입니다."""
+
 def say_hello(name):
     print("Hello,", name)
+
+def read_file(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        return f.read()
+
+if __name__ == "__main__":
+    say_hello("github actions")
